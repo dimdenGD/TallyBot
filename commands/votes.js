a5d24f813d91dbb4011710269911f037585099e3
dimden <admin@dimden.dev>
2020-07-22 01:45:02 +0300

    Votes

---
+++ commands/votes.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+
+module.exports.run = (msg, args, bot, db, isAdmin) => {
+    let page = parseInt(args[0]);
+    if(isNaN(page)) page = 1;
+
+    let songs = db.db.prepare(`select * from songs order by votes desc limit ${(page-1)*10}, 10`).all();
+
+    let i = ((page-1)*10)+1;
+
+    let embed = new Discord.RichEmbed()
+        .setAuthor(msg.author.tag, msg.author.displayAvatarURL)
+        .setTitle(`Song leaderboard by votes`)
+        .setDescription(songs.map(s => `${i++}. ${s.name} • ${s.votes} votes`).join("\n"))
+        .setColor("RANDOM")
+        .setFooter(`${page} page`)
+        .setTimestamp();
+
+    msg.channel.send(embed);
+};
+module.exports.aliases = [];
