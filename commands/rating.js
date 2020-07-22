3cc9c7909bdabb385b4020c8c0964058afe57636
dimden <admin@dimden.dev>
2020-07-22 12:39:27 +0300

    Rating

---
+++ commands/rating.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+
+module.exports.run = (msg, args, bot, db, isAdmin) => {
+    let page = parseInt(args[0]);
+    if(isNaN(page)) page = 1;
+
+    let songs = db.db.prepare(`select id, name from songs`).all();
+
+    for(let i in songs) {
+        let song = songs[i];
+        let rating = db.getRating(song.id);
+        if(!isNaN(parseFloat(rating))) song.rating = parseFloat(rating);
+        else song.rating = 0;
+    }
+    songs = songs.sort((a, b) => b.rating-a.rating);
+    console.log(songs);
+    songs = songs.slice((page-1)*10, ((page-1)*10)+10);
+
+    let i = ((page-1)*10)+1;
+
+    let embed = new Discord.RichEmbed()
+        .setAuthor(msg.author.tag, msg.author.displayAvatarURL)
+        .setTitle(`Song leaderboard by rating`)
+        .setDescription(songs.map(s => `${i++}. ${s.name} • ${s.rating} ⭐`).join("\n"))
+        .setColor("RANDOM")
+        .setFooter(`${page} page`)
+        .setTimestamp();
+
+    msg.channel.send(embed);
+};
+module.exports.aliases = [];
